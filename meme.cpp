7e8d1173c18004f5182b1ae6b861b4c9eeea4640
mcarri01 <matthew.carrington_fair@tufts.edu>
2017-10-14 02:52:53 -0400

    wrapper for calling interpreter

---
+++ meme.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	cout << argv[1];
+	string file = argv[1];
+
+	string command = "python meme_int.py ";
+	command.append(file); 
+	const char* conv_command = command.c_str();
+	system(conv_command);
+	return 0; 
+
+}
